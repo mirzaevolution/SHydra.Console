5e83581100484333bf0fe126d6c50914e73086c3
Mirza Ghulam Rasyid <mirzaevolution@gmail.com>
2019-08-09 14:37:52 +0700

    Add project files.

---
+++ SHydra.Console/CommandHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SHydra.Console
+{
+    public class CommandHandler
+    {
+        public static void ExecuteCommand(string[] args)
+        {
+
+        }
+    }
+}
